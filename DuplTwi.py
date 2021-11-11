44b3db57f7c6c9d141f15a99b0a068db897e0a76
cremans <88071871+cremans@users.noreply.github.com>
2021-11-11 16:41:42 +0000

    Update DuplTwi.py

--- DuplTwi.py
+++ DuplTwi.py
@@ -280,7 +280,7 @@ def main(vodID):
         os.system('winrar.exe a -afrar -df -m5 -mt3 -ri15 -t -tk -ts -v14g "' + nom_vid +
                   '.rar" "' + nom_vid + '.mp4"')
 
-main("1145743889")
-main("1146639753")
-main("1147579429")
-main("1148495446")
+main("1149433317")
+main("1150353644")
+main("1153319957")
+main("1154195108")
